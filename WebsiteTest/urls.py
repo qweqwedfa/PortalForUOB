fc7ea66be44105792b263fb4b042531c49883a92
Yhj <yhjdeyouxiang@gmail.com>
2025-02-04 22:53:39 +0000

    SQl training

--- WebsiteTest/urls.py
+++ WebsiteTest/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
- 
+from django.contrib import admin
+from django.urls import path, include
 from . import views
- 
+from .views import item_list
+
 urlpatterns = [
-    path("", views.hello, name="hello"),
+    path('admin/', admin.site.urls),
+    # path('', include('WebsiteTest.urls')),
+    # path("", views.hello, name="hello"),
+    path("", item_list, name="item_list"),
     path("portal_test_1/", views.portal_test, name="portal_test"),
     path("demo/", views.demo, name="demo_test"),
-] 
+    #path('../../reder_demo/', include('reder_demo.urls')),
+]
--- WebsiteTest/views.py
+++ WebsiteTest/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Item
 
 def hello(request):
     text_ = "Hello world !\nThe test website for IBM portal 1."
@@ -9,4 +11,17 @@ def portal_test(request):
     return render(request, "PortalTest.html")
 
 def demo(request):
-    return render(request, "demo.html")
+    return render(request, "render_demo.html")
+
+
+
+def item_list(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        if name and description:
+            Item.objects.create(name=name, description=description)
+        return redirect("item_list")  # 避免重复提交表单
+
+    items = Item.objects.all()  # 查询数据库中的所有对象
+    return render(request, "../templates/render_demo.html", {"items": items})
